5065dfaae5beb4db31459c494eff729dc1b8affe
KyotaHoshi <kyota-h.8@outlook.jp>
2020-02-26 04:19:45 +0900

    isDoneボタン完成

--- src/Item.js
+++ src/Item.js
@@ -3,37 +3,51 @@ import React, { useState } from "react"
 const Item = ({ todo, todos, setTodos, }) => {
     // console.log(todo);
 
+    const isDoneText = (x) => {
+        if (x.isDone === true) {
+            return "完了"
+        } else {
+            return "未完了"
+        }
+    }
+    
+    const [isDone, setIsDone] = useState(isDoneText(todo))
+
     const handleDelete = () => {
         const newTodos = todos.slice()
         newTodos.splice(todo.id, 1)
-        console.log(todo.id);
+        // console.log(todo.id);
         
-
         // id振り直し
         newTodos.map((x, i) => {
+            setIsDone(isDoneText(x))
             return (
                 x.id = i
             )
         })
-
+            
         setTodos(newTodos)
+        console.log("-----Delete------");
+        console.log(todos);
+        console.log(todo);
     }
 
-    const [isDone, setIsDone] = useState("未完了")
-
+    
     const handleIsDone = () => {
         const doneTodo = todos.find(x => x.id === todo.id)
         if ( doneTodo.isDone === false){
             doneTodo.isDone = true
-            setIsDone("完了")
+            // setIsDone("完了")
         } else {
             doneTodo.isDone = false
-            setIsDone("未完了")
+            // setIsDone("未完了")
         }
-        console.log(todo.isDone);
+        setIsDone(isDoneText(todo))
+        console.log("-----isDone------");
+        console.log(todo);
         console.log(todos);
-
     }
+
 
     return (
             <>
